47aaf128f1d9e25c31737fb7260c6b34f049f2d5
PankovDaniil <113036456+PankovDaniil@users.noreply.github.com>
2022-12-26 03:05:43 +0300

    Update Theory.cs

--- Theory.cs
+++ Theory.cs
@@ -354,9 +354,9 @@ namespace Lab_5
             #region Num_2
             {
                 double a1 = 15, b1 = 20, c1 = 25, a2 = 16, b2 = 30, c2 = 34;
-                if (a1 > 0 & a2 > 0 & b1 > 0 & b2 > 0 & c1 > 0 & c2 > 0)
+                if (a1 > 0 && a2 > 0 && b1 > 0 && b2 > 0 && c1 > 0 && c2 > 0)
                 {
-                    if ((a1 + b1 > c1) & (a1 + c1 > b1) & (b1 + c1 > a1) & (a2 + b2 > c2) & (a2 + c2 > b2) & (b2 + c2 > a2))
+                    if ((a1 + b1 > c1) && (a1 + c1 > b1) && (b1 + c1 > a1) && (a2 + b2 > c2) && (a2 + c2 > b2) && (b2 + c2 > a2))
                     {
                         if (Square(a1, b1, c1) > Square(a2, b2, c2))
                         {
