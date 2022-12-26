db015521a90404ca5f5261affa471334870feb1a
DmitriiLavrentev <113678491+DmitriiLavrentev@users.noreply.github.com>
2022-12-26 09:59:22 +0300

    LB5
    Fixed

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Transactions;
 
-
+/*
 #region lvl1 ex1
 static void C(int n)
 {
@@ -257,9 +257,124 @@ Console.WriteLine(ind1);
 Console.WriteLine(ind2);
 Result(res(ind1, ind2, ar));
 #endregion
-
+*/
 
 #region lvl2 ex23
+
+static List<int> maxim(List<List<int>> matrix, List<int> maxi)
+{
+    int max = matrix[0][0];
+    for (int i = 0; i < matrix.Count; i++)
+    {
+        for (int j = 0; j < matrix[i].Count; j++)
+        {
+            if (matrix[i][j] > max && maxi.Contains(matrix[i][j]) == false)
+            {
+                max = matrix[i][j];
+            }
+        }
+    }
+    maxi.Add(max);
+    return maxi;
+}
+static void vvodmatrici(int n, int m, out List<List<int>> matrix)
+{
+    matrix = new List<List<int>>();
+    string[] line;
+    for (int i = 0; i < n; i++)
+    {
+        matrix.Add(new List<int>());
+        while (true)
+        {
+            line = Console.ReadLine().Split(" ");
+            if (line.Length != m) { Console.WriteLine("re-enter the line: "); continue; }
+            else { break; }
+        }
+
+        for (int j = 0; j < m; j++)
+        {
+            int x;
+            int.TryParse(line[j], out x);
+            matrix[i].Add(x);
+        }
+    }
+
+}
+static void vivodmatrici(List<List<int>> matrix)
+{
+    for (int i = 0; i < matrix.Count(); i++)
+    {
+        for (int j = 0; j < matrix[0].Count(); j++)
+        {
+            Console.Write(matrix[i][j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+static List<List<int>> preobr1(List<List<int>> matrix, List<int> maxi)
+{
+    for (int i = 0; i < matrix.Count; i++)
+    {
+        for (int j = 0; j < matrix[0].Count(); j++)
+        {
+            if (maxi.Contains(matrix[i][j]) == true) matrix[i][j] = matrix[i][j] * 2;
+            else matrix[i][j] = matrix[i][j] / 2;
+        }
+    }
+    return matrix;
+}
+
+List<List<int>> matrix1 = new List<List<int>>();
+List<List<int>> matrix2 = new List<List<int>>();
+Console.WriteLine("Enter the first size of first matrix: ");
+int n1;
+int.TryParse(Console.ReadLine(), out n1);
+if (n1 <= 0) { Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
+Console.WriteLine("Enter the second size of first matrix: ");
+int m1;
+int.TryParse(Console.ReadLine(), out m1);
+if (m1 <= 0) { Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
+Console.WriteLine("Enter first matrix: ");
+vvodmatrici(n1, m1, out matrix1);
+Console.WriteLine("Enter the first size of second matrix: ");
+int n2;
+int.TryParse(Console.ReadLine(), out n2);
+if (n2 <= 0) { Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
+Console.WriteLine("Enter the second size of second matrix: ");
+int m2;
+int.TryParse(Console.ReadLine(), out m2);
+if (m2 <= 0) { Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
+Console.WriteLine("Enter second matrix: ");
+vvodmatrici(n2, m2, out matrix2);
+List<int> maxi1 = new List<int>();
+List<int> maxi2 = new List<int>();
+
+
+for (int i = 0; i < 5; i++)
+{
+    maxim(matrix1, maxi1);
+    maxim(matrix2, maxi2);
+}
+
+matrix1 = preobr1(matrix1, maxi1);
+matrix2 = preobr1(matrix2, maxi2);
+
+Console.WriteLine("New first matrix: ");
+vivodmatrici(matrix1);
+Console.WriteLine("New second matrix: ");
+vivodmatrici(matrix2);
+
+return 0;
+
+#endregion
+/*
+#region lvl3 ex2
+
+Resul oper = AR;
+double[][] ara = QWE();
+double[][] resut = oper(ara);
+Result(resut);
+
 static double[][] QWE()
 {
     int x = 0;
@@ -290,61 +405,6 @@ static double[][] QWE()
         return flag;
     }
 }
-
-static double[][] Maximum(double[][] arr1)
-{
-    (int, int)[] inds = new (int, int)[5];
-    for (int i = 0; i < inds.Length; i++)
-    {
-        (int, int) max = (0, 0);
-        for (int k = 0; k < arr1.Length; k++)
-        {
-            for (int j = 0; j < arr1[k].Length; j++)
-            {
-                if (inds.Contains((k, j)))
-                    continue;
-                if (arr1[k][j] > arr1[max.Item1][max.Item2])
-                    max = (k, j);
-            }
-        }
-        inds[i] = max;
-    }
-    Console.WriteLine(string.Join(", ", inds));
-
-    for (int i = 0; i < arr1.Length; i++)
-        for (int j = 0; j < arr1[i].Length; j++)
-        {
-            if (inds.Contains((i, j)))
-            {
-                arr1[i][j] *= 2;
-            }
-            else
-            {
-                arr1[i][j] /= 2;
-            }
-        }
-    return arr1;
-}
-
-double[][] arr1 = QWE();
-double[][] arr2 = QWE();
-
-Result(arr1);
-Console.WriteLine("-----------------------------------------------------");
-Result(Maximum(arr1));
-Console.WriteLine("-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-");
-Result(arr2);
-Console.WriteLine("-----------------------------------------------------");
-Result(Maximum(arr2));
-#endregion
-
-#region lvl3 ex2
-
-Resul oper = AR;
-double[][] ara = QWE();
-double[][] resut = oper(ara);
-Result(resut);
-
 
 static double[][] AR(double[][] a)
 {
@@ -425,3 +485,4 @@ class HelloWorld
     }
 }
 #endregion
+*/
